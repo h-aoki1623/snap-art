045be2c4013d8b741acd97e6f8f5aa604b7674d8
h-aoki1623 <aokin.skywalker.c1623@gmail.com>
2019-01-12 23:29:10 +0900

    Update x-csrf-token settings

--- config/application.rb
+++ config/application.rb
@@ -15,17 +15,10 @@ module SnapArt
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Credentials' => 'true',
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => '*'
-    }
-
-    #config.middleware.insert_before 0, Rack::Cors do
-      #allow do
-        #origins '*'
-        #resource '*', :headers => :any, :methods => [:get, :post, :options]
-      #end
-    #end
+    #config.action_dispatch.default_headers = {
+      #'Access-Control-Allow-Credentials' => 'true',
+      #'Access-Control-Allow-Origin' => '*',
+      #'Access-Control-Request-Method' => '*'
+    #}
   end
 end
