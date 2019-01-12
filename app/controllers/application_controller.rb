811708688c164eb68ddbf7aff3eb0d5306c7b824
h-aoki1623 <aokin.skywalker.c1623@gmail.com>
2019-01-12 21:38:08 +0900

    Update x-csrf-token settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :http_header_log
+
+  private
+
+    def http_header_log
+      logger.info("api_version:#{request.headers[:HTTP_API_VERSION]}")
+    end
 end
